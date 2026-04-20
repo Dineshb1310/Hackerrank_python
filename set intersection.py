c9657d1e601b1d2319d91e7902477de181cf6d12
DINESH B <dineshbackiyaraj_bec28@mepcoeng.ac.in>
2026-04-20 11:57:05 +0530

    Refactor set intersection.py by cleaning up code
    Remove comments and unnecessary input lines from the code.

--- set intersection.py
+++ set intersection.py
@@ -1,13 +1,7 @@
-# English newspaper subscribers
 _ = input() 
 english_set = set(map(int, input().split()))
 
-# French newspaper subscribers
 _ = input() 
 french_set = set(map(int, input().split()))
-
-# Find students in both sets using the intersection operator
 both_subscriptions = english_set & french_set
-
-# Output the total count
 print(len(both_subscriptions))
